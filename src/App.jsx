e17b48b0adcc0d8da28325d5da65a4f276e1cf91
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2024-04-02 13:23:55 +0100

    Home.jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact';
+import HomeMain from './pages/HomeMain';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<HomeMain />} />
         <Route path='/About' element={<About />} />
         <Route path='/Projects' element={<Projects />} />
         <Route path='/Contact' element={<Contact />} />
---
+++ src/pages/HomeMain.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './Home/Home';
+import About from './About/About';
+import Projects from './Projects/Projects';
+import Contact from './Contact/Contact';
+
+function HomeMain() {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default HomeMain;
